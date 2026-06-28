fd66b3b2c363aba13b8a8f713cfe0488516bffcd
Abhinav <fanvkabhi@gmail.com>
2026-06-28 23:50:25 +0530

    squaring a sorted array (with negative inputs)

---
+++ Squaresort.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Squaresort {
+    public int[] sortedSquares(int[] nums) {
+        int n=nums.length;
+        int[] newarr=new int[n];
+        int i=0;
+        int j=n-1;
+        int k=n-1;
+        while(i<=j){
+            int leftsq=nums[i] * nums[i];
+            int rightsq=nums[j] * nums[j];
+            if(leftsq>rightsq){
+                newarr[k--]=leftsq;
+                i++;
+            }
+            else{
+                newarr[k--]=rightsq;
+                j--;
+            }
+        }
+        return newarr;
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Squaresort solution=new Squaresort();
+        int[] k=solution.sortedSquares(arr);
+        for(int i=0;i<n;i++){
+            System.out.print(k[i]+" ");
+        }
+    }
+}
